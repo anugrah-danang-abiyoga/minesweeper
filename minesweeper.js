ff7269b25b00ebc3cb21d59e419e5e27ce452bf6
anugrah-danang <danangabiyoga26@gmail.com>
2018-02-17 23:12:59 +1300

    initial commit

--- minesweeper.js
+++ minesweeper.js
@@ -1,11 +1,21 @@
+
 document.addEventListener('DOMContentLoaded', startGame)
 
 // Define your `board` object here!
-var board = {} 
+var board = {}
+board.cells = [
+ {row: 0, col: 0, isMine: false, hidden: true}, 
+ {row: 0, col: 1, isMine: true, hidden: true}, 
+ {row: 0, col: 2, isMine: false, hidden: true}, 
+ {row: 1, col: 0, isMine: true, hidden: true},
+ {row: 1, col: 1, isMine: false, hidden: true},
+ {row: 1, col: 2, isMine: true, hidden: true},
+ {row: 2, col: 0, isMine: false, hidden: true},
+ {row: 2, col: 1, isMine: false, hidden: true},
+ {row: 2, col: 2, isMine: true, hidden: true}]
+ 
 var boardDimension = 5;
-var initialTime;
-var timeInterval; 
-var timeStarted = false;
+
 function startGame () {
   // Don't remove this function call: it makes the game work!
   lib.initBoard()
